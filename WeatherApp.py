3df8780be6b2a8e88f6d8f7eac53e877cefbf904
SREENATH G <sreenath2410530@ssn.edu.in>
2025-07-16 18:26:04 +0530

    WeatherApp.py
    Signed-off-by: SREENATH G <sreenath2410530@ssn.edu.in>

--- WeatherApp.py
+++ WeatherApp.py
@@ -26,5 +26,5 @@ def index():
     return render_template('index.html', weather=weather)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))r
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
